7021a2ea46ec23e962d7eaaab8e8f00b1274b572
anna shcherbina <annashcherbina@gmail.com>
2019-02-17 17:26:04 -0800

    named versions for all dependencies

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ config = {
     'author_email': 'annashch@stanford.edu',
     'url': 'https://github.com/kundajelab/seqdataloader',
     'description': 'Generate genome-wide classification and regression labels for DNA accessibility data.',
-    'version': '0.116',
+    'version': '0.117',
     'packages': ['seqdataloader'],
     'setup_requires': [],
-    'install_requires': ['numpy','pandas','cython','deeptools','pybedtools','pyBigWig'],
+    'install_requires': ['numpy>=1.15','pandas>=0.23.4','cython>=0.27.3','deeptools>=3.0.1','pybedtools>=0.7','pyBigWig>=0.3.2'],
     'scripts': [],
     'entry_points': {'console_scripts': ['genomewide_labels=seqdataloader.__init__:main']},
     'name': 'seqdataloader'
