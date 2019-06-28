28a9d7576382a2c957dfb1784667087f74df1cdb
Av Shrikumar <avanti.shrikumar@gmail.com>
2019-06-28 11:33:45 -0700

    Bugfix

--- seqdataloader/batchproducers/coordbased/coordstovals/bigwig.py
+++ seqdataloader/batchproducers/coordbased/coordstovals/bigwig.py
@@ -77,10 +77,24 @@ class AbstractPosAndNegStrandCountsAndProfile(CoordsToVals):
             new_coors.append(Coordinates(chrom=coor.chrom,
                                          start=new_start, end=new_end,
                                          isplusstrand=coor.isplusstrand))
-        pos_profile_values = self.pos_strand_reader.read_values(
+        first_strand_profile_values = self.pos_strand_reader.read_values(
                                   coors=new_coors)
-        neg_profile_values = np.abs(
+        second_strand_profile_values = np.abs(
             self.neg_strand_reader.read_values(coors=new_coors))
+        pos_strand_profile_values = []
+        neg_strand_profile_values = []
+        #need to swap the pos and neg strands if the strand is negative
+        for (first_strand, second_strand, coor) in zip(first_strand_profile_values,
+                                                       second_strand_profile_values,
+                                                       coors):
+            if (coor.isplusstrand==True):
+                pos_strand_profile_values.append(first_strand)
+                neg_strand_profile_values.append(second_strand)
+            else:
+                pos_strand_profile_values.append(second_strand)
+                neg_strand_profile_values.append(first_strand)
+        pos_strand_profile_values = np.array(pos_strand_profile_values)
+        neg_strand_profile_values = np.array(neg_strand_profile_values)        
         pos_counts = np.sum(pos_profile_values, axis=-1)
         neg_counts = np.sum(neg_profile_values, axis=-1)
         return (pos_counts, neg_counts, pos_profile_values, neg_profile_values)
