259d7557c6f056ec7b2b471d029a1cfa983a76b0
Anon <blindauth>
2019-09-03 12:32:13 -0700

    bugfix

--- seqdataloader/batchproducers/coordbased/coordstovals/lookup.py
+++ seqdataloader/batchproducers/coordbased/coordstovals/lookup.py
@@ -11,8 +11,8 @@ class SimpleLookup(AbstractSingleNdarrayCoordsToVals):
         self.default_returnval = default_returnval
         self.lookup = {}
         self.num_labels = None
-        for line in (gzip.open(self.bed_file) if ".gz"
-                    in self.bed_file else open(self.bed_file)):
+        for line in (gzip.open(self.lookup_file) if ".gz"
+                     in self.lookup_file else open(self.lookup_file)):
             (chrom, start_str, end_str, *labels) =\
               line.decode("utf-8").rstrip().split("\t")[0:3]
             coord = Coordinates(chrom=chrom,
