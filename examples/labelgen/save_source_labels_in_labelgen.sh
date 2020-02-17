e71a077b77b1bd04711451dd974c544f335530e1
anna shcherbina <annashcherbina@gmail.com>
2020-02-17 12:28:59 -0800

    example of saving source peaks for genomewide bins

---
+++ examples/labelgen/save_source_labels_in_labelgen.sh
@@ -0,0 +1,14 @@
+genomewide_labels --task_list tasks.labelgen.tsv \
+		  --outf classificationlabels.SummitWithin200bpCenter.tsv.gz \
+		  --output_type gzip \
+		  --chrom_sizes hg38.chrom.sizes \
+		  --bin_stride 50 \
+		  --left_flank 400 \
+		  --right_flank 400 \
+		  --bin_size 200 \
+		  --chrom_threads 10 \
+		  --task_threads 4 \
+		  --allow_ambiguous \
+		  --labeling_approach peak_summit_in_bin_classification \
+		  --save_label_source
+
