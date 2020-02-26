2eaaaa5ea44b2ed96739a349a27147be889c932e
anna shcherbina <annashcherbina@gmail.com>
2020-02-26 12:11:07 -0800

    updates to encode attribute configuration to include unstranded 5p count track

--- seqdataloader/dbingest/attrib_config.py
+++ seqdataloader/dbingest/attrib_config.py
@@ -34,7 +34,12 @@ def get_encode_config():
     attrib_info['count_bigwig_minus_5p']={'dtype':'float32',
                                           'opener':open_bigwig_for_parsing,
                                           'parser':parse_bigwig_chrom_vals,
-                                          'store_summits':False}    
+                                          'store_summits':False}
+    attrib_info['count_bigwig_unstranded_5p']={'dtype':'float32',
+                                          'opener':open_bigwig_for_parsing,
+                                          'parser':parse_bigwig_chrom_vals,
+                                          'store_summits':False}
+    
     attrib_info['idr_peak']={'dtype':'int',
                              'opener':open_csv_for_parsing,
                              'parser':parse_narrowPeak_chrom_vals,
