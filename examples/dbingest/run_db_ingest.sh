333c5472f317f1e262a20451bf90f3ee28cbb9a1
anna shcherbina <annashcherbina@gmail.com>
2020-05-11 16:47:58 -0700

    updated example

--- examples/dbingest/run_db_ingest.sh
+++ examples/dbingest/run_db_ingest.sh
@@ -1,8 +1,15 @@
-db_ingest --tiledb_metadata tasklist.tsv \
-	--tiledb_group highmem_50million_write_chunk \
-	--overwrite \
-	--chrom_sizes hg38.chr10.size \
-	--task_threads 2 \
-	--chrom_threads 25 \
-	--write_threads 1 \
-	--attribute_config encode_pipeline
+#!/bin/bash
+#rm -r db/dnase
+
+db_ingest --tiledb_metadata tier1.encode.dnase.tasks.tsv \
+	  --array_name db/dnase \
+	  --overwrite \
+	  --chrom_sizes hg38.chrom.sizes \
+	  --attribute_config encode_pipeline \
+	  --coord_tile_size 10000 \
+	  --task_tile_size 1 \
+	  --write_chunk 30000000 \
+	  --threads 20 \
+	  --max_queue_size 50 \
+	  --max_mem_g 200
+
